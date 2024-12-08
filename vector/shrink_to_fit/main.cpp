8d12c9bdc6764b948d08e452d75547429ef3634f
innno <banxiaqaq@gmail.com>
2024-12-08 17:01:55 +0800

    1

---
+++ vector/shrink_to_fit/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    std::vector<int> v;
+    std::cout << "Default-constructed capacity is " << v.capacity() << '\n';
+    v.resize(100);
+    std::cout << "Capacity of a 100-element vector is " << v.capacity() << '\n';
+    v.resize(50);
+    std::cout << "Capacity after resize(50) is " << v.capacity() << '\n';
+    v.shrink_to_fit();
+    std::cout << "Capacity after shrink_to_fit() is " << v.capacity() << '\n';
+    v.clear();
+    std::cout << "Capacity after clear() is " << v.capacity() << '\n';
+    v.shrink_to_fit();
+    std::cout << "Capacity after shrink_to_fit() is " << v.capacity() << '\n';
+    for (int i = 1000; i < 1300; ++i)
+        v.push_back(i);
+    std::cout << "Capacity after adding 300 elements is " << v.capacity() << '\n';
+    v.shrink_to_fit();
+    std::cout << "Capacity after shrink_to_fit() is " << v.capacity() << '\n';
+    return 0;
+}
