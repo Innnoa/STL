e048ad0201effd6d76e01ec369c0077e7b5b0166
innno <banxiaqaq@gmail.com>
2024-12-08 15:59:58 +0800

    1

---
+++ vector/empty/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cout<<boolalpha;
+    vector<int> numbers;
+    cout<< numbers.empty()<<endl;
+    numbers.push_back(42);
+    cout<<numbers.empty()<<endl;
+    return 0;
+}
