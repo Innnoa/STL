d3adb787f88643583d483d5416d649805f35a9ce
innno <banxiaqaq@gmail.com>
2024-12-08 16:02:29 +0800

    1

---
+++ vector/size/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    vector<int> nums;
+    assert(nums.size()==0);
+    nums={1,2,3,4};
+    assert(nums.size()==4);
+    return 0;
+}
