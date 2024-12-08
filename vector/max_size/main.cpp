14c7133871fb3936e88e0a23ed6bc1f2d88c6760
innno <banxiaqaq@gmail.com>
2024-12-08 16:25:25 +0800

    1

---
+++ vector/max_size/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    constexpr vector<char> p;
+    constexpr vector<long> q;
+    cout << uppercase << "p.max_size()= " << dec << p.max_size() << "= 0x" << hex << p.max_size() << endl <<
+            "q.max_size()= " << dec << q.max_size() << "= 0x" << hex << q.max_size() << endl;
+    return 0;
+}
