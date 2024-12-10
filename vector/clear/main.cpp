1aab5a5517e3d843c7f186694e8b3731ecd6f4a6
innno <banxiaqaq@gmail.com>
2024-12-10 15:29:06 +0800

    1

--- vector/clear/main.cpp
+++ vector/clear/main.cpp
@@ -9,7 +9,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print_info(const string_view rem,const vector<int>& v) {
+    cout<<rem<<"{";
+    for (const auto & i:v)
+        {
+            cout<<i<<" ";
+        }
+    cout<<"}\n";
+    cout<<"Size="<<v.size()<<", Capacity="<<v.capacity()<<"\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    vector container{1,2,3};
+    print_info("Before clear: ",container);
+    container.clear();
+    print_info("After clear: ",container);
     return 0;
 }
