42ab53aeef8d501db781fc37ea6f8c8aeacd8d0e
innno <banxiaqaq@gmail.com>
2024-12-11 17:59:36 +0800

    1

--- array/get/main.cpp
+++ array/get/main.cpp
@@ -8,8 +8,14 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
-
+constexpr array v{1,2,3};
+static_assert(get<0>(v)==1&&get<1>(v)==2&&get<2>(v)==3);
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    array<int ,3>a{};
+    //set values:
+    get<0>(a)=1,get<1>(a)=2,get<2>(a)=3;
+    //get values;
+    cout<<'('<<get<0>(a)<<','<<get<1>(a)<<','<<get<2>(a)<<")\n";
     return 0;
 }
