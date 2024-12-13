601eba90e9f1f785ea17b963d91a32499308a8c0
innno <banxiaqaq@gmail.com>
2024-12-13 17:11:03 +0800

    1

--- array/rend+crend/main.cpp
+++ array/rend+crend/main.cpp
@@ -11,14 +11,15 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    array a{1,11,11,35,0,12,79,76,76,69,40};
+    array a{1, 11, 11, 35, 0, 12, 79, 76, 76, 69, 40};
     //Print elements of container in reverse order using const_reverse_iterator`s.
-    for_each(a.rbegin(),a.rend(),[](auto e){cout<<e<<" ";});
-    cout<<"\n";
+    for_each(a.rbegin(), a.rend(), [](auto e) { cout << e << " "; });
+    cout << "\n";
+    for (int i=0;i<=7;i++)
     //Modify each element of container using non-const reverse_iterator`s.
-    for_each(a.rbegin(),a.rend(),[](auto & e){e+=32;});
+    for_each(a.rbegin(), a.rend(), [](auto &e) { e += 32; });
     //Print elements as chars in reverse order using const_reverse_iterator`s.
-    for_each(a.rbegin(),a.rend(),[](auto e){cout<<e<<" ";});
-    cout<<"\n";
+    for_each(a.rbegin(), a.rend(), [](auto e) { cout << e << " "; });
+    cout << "\n";
     return 0;
 }
