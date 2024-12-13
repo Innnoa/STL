20187ad3d19956abf138cf104b4d0f22063ec97d
innno <banxiaqaq@gmail.com>
2024-12-13 19:49:30 +0800

    1

--- deque/shrink_to_fit/main.cpp
+++ deque/shrink_to_fit/main.cpp
@@ -9,7 +9,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Minimal c++11 allocator with debug output.
+template<class Tp>
+struct NAlloc
+{
+    typedef Tp value_type;
+    NAlloc() = default;
+    template<class T> explicit NAlloc(const NAlloc<T>&) {}
+
+    static Tp* allocate(size_t n) {
+        n*=sizeof(Tp);
+        cout<<"allocating"<<n<<" bytes\n";
+        return static_cast<Tp*>(::operator new(n));
+    }
+
+    static void deallocate(Tp* p,std::size_t n) {
+        cout<<"deallocating "<<n*sizeof*p<<" bytes\n";
+        ::operator delete(p);
+    }
+};
+
+template<class T,class U>
+bool operator == (const NAlloc<T>&,const NAlloc<U>&){return true;}
+template<class T,class U>
+bool operator != (const NAlloc<T>&,const NAlloc<U>&){return false;}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cout<<"Default-construct deque:\n";
+    deque<int,NAlloc<int>> deq;
+    cout<<"\nAdd 300 elements:\n";
+    for (auto i=1000;i<1300;i++)
+        {
+            deq.push_back(i);
+        }
+    cout<<"\npop 100 elements:\n";
+    for (auto i=0;i<100;i++)
+        {
+            deq.pop_front();
+        }
+    cout<<"\nRun shrink_to_fit:\n";
+    deq.shrink_to_fit();
+    cout<<"\nDestory deque as it goes out of scope:\n";
+
     return 0;
 }
