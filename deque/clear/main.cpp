5f09b7698ba7ab24aee6234a515ea74edef74a12
innno <banxiaqaq@gmail.com>
2024-12-15 14:37:35 +0800

    1

--- deque/clear/main.cpp
+++ deque/clear/main.cpp
@@ -9,7 +9,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print_deq(const auto & str,const auto & deq) {
+
+    for (cout<<str<<": {";const auto & i:deq)
+        {
+            cout<<i<<" ";
+        }
+    cout<<"}Size: "<<deq.size()<<"\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    deque deq{1,2,3,4};
+    print_deq("before",deq);
+    deq.clear();
+    print_deq("after",deq);
     return 0;
 }
