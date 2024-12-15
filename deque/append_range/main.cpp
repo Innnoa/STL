b7f5026c758200af1455e61a664856122f41a29d
innno <banxiaqaq@gmail.com>
2024-12-15 16:29:16 +0800

    1

--- deque/append_range/main.cpp
+++ deque/append_range/main.cpp
@@ -11,5 +11,14 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto head = deque{1, 2, 3, 4};
+    auto li = list{5, 6, 7};
+#ifdef __cpp_lib_containers_ranges
+    head.append_range(list);
+#else
+    head.insert(head.end(), li.begin(), li.end());
+#endif
+    assert((head == deque{1,2,3,4,5,6,7}));
+
     return 0;
 }
