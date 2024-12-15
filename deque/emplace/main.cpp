1c36077417c075aa68ed6ffaee4105bf18a8bb4a
innno <banxiaqaq@gmail.com>
2024-12-15 15:39:05 +0800

    1

--- deque/emplace/main.cpp
+++ deque/emplace/main.cpp
@@ -9,7 +9,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct A {
+    string s;
+    explicit A(string str):s(move(str)){cout<<"Constructed\n";}
+    A(const A & a):s(a.s){cout<<"copy Constructed";}
+    A(A&& a) noexcept :s(move(a.s)){cout<<"move Constructed";}
+    A& operator= (const A& other) {
+        s=other.s;
+        cout<<"copy assigned\n";
+        return *this;
+    }
+    A& operator= (A&& other)  noexcept {
+        s=move(other.s);
+        cout<<"move assigned\n";
+        return *this;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    deque<A> deq;
+    A two("two");
+    A three("three");
+    cout<<"emplace: \n";
+    deq.emplace(deq.end(),"one");
+    cout<<"emplace with copy: \n";
+    deq.emplace(deq.end(),two);
+    cout<<"emplace with move: \n";
+    deq.emplace(deq.end(),move(three));
+    cout<<"content: \n";
+    for (const auto & i:deq)
+        {
+            cout<<i.s<<" ";
+        }
     return 0;
 }
