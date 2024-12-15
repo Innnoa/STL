1392c2fb4e0ec87b06005eb879867d9393d9c4d4
innno <banxiaqaq@gmail.com>
2024-12-15 17:22:26 +0800

    1

---
+++ deque/swap/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class Os, class Co>
+Os& operator<<(Os& os, const Co& co)
+{
+    os << '{';
+    for (auto const& i : co)
+        os << ' ' << i;
+    return os << " } ";
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    std::deque<int> a1{1, 2, 3}, a2{4, 5};
+
+    auto it1 = std::next(a1.begin());
+    auto it2 = std::next(a2.begin());
+
+    int& ref1 = a1.front();
+    int& ref2 = a2.front();
+
+    std::cout << a1 << a2 << *it1 << ' ' << *it2 << ' ' << ref1 << ' ' << ref2 << '\n';
+    a1.swap(a2);
+    std::cout << a1 << a2 << *it1 << ' ' << *it2 << ' ' << ref1 << ' ' << ref2 << '\n';
+    return 0;
+}
