6754756cd55e796da450d8c1106925759409a863
innno <banxiaqaq@gmail.com>
2024-12-16 17:24:21 +0800

    1

---
+++ list/back/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis = list {1,2,3,4};
+    cout<<lis.back();
+    return 0;
+}
