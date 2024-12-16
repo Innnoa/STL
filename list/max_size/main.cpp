dcea4b278388b3911c85fa704d03c11b2f4f9c1d
innno <banxiaqaq@gmail.com>
2024-12-16 17:35:52 +0800

    1

---
+++ list/max_size/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    std::list<char> p;
+    std::list<long> q;
+    std::cout << std::uppercase
+              << "p.max_size() = " << std::dec << p.max_size() << " = 0x"
+              << std::hex << p.max_size() << '\n'
+              << "q.max_size() = " << std::dec << q.max_size() << " = 0x"
+              << std::hex << q.max_size() << '\n';
+
+    return 0;
+}
