2f664924d4a857313dcd45c66b7a58ec7ca94b08
innno <banxiaqaq@gmail.com>
2024-12-16 17:34:45 +0800

    1

---
+++ list/size/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis = list {1,2,3,4};
+    cout<<"before:"<<lis.size()<<"\n";
+    lis.assign(5,8);
+    cout<<"after:"<<lis.size();
+    return 0;
+}
