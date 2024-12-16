568d2bbce8c230bdc72c0b275663f00dc338ad2b
innno <banxiaqaq@gmail.com>
2024-12-16 16:17:08 +0800

    1

--- list/list_constructor/main.cpp
+++ list/list_constructor/main.cpp
@@ -9,7 +9,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename T>
+ostream& operator<<(ostream& s,const list<T>& v) {
+    s.put('{');
+    for (char comma[]{'\0', ' ','\0'};const auto & e:v)
+        {
+            s<<comma<<e,comma[0]=',';
+        }
+    return s<<"}\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    list<string> lis1{"the", "frogurt","is","also","cursed"};
+    cout<<"1: "<<lis1;
+    list lis2(lis1.begin(),lis1.end());
+    cout<<"2: "<<lis2;
+    list lis3(lis1);
+    cout<<"3: "<<lis3;
+    list<string> lis4(5,"Mo");
+    cout<<"4: "<<lis4;
+    auto const rg = {"cat","dog","bird"};
+#ifdef __cpp_lib_containers_ranges
+    list<string> lis5(from_range,rg);
+#else
+    list<string> lis5(rg.begin(),rg.end());
+#endif
+    cout<<"5: "<<lis5;
     return 0;
 }
