2faa15acb65a765e757c91059127b7126ece8fef
innno <banxiaqaq@gmail.com>
2024-12-16 17:33:07 +0800

    1

--- list/empty/main.cpp
+++ list/empty/main.cpp
@@ -11,5 +11,9 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis = list<int>{};
+    cout<<"before:"<<lis.empty()<<"\n";
+    lis.assign(1,2);
+    cout<<"after:"<<lis.empty();
     return 0;
 }
