7ecb2b2562882172609554f7c64543a50d9f97fd
innno <banxiaqaq@gmail.com>
2024-12-17 09:57:55 +0800

    1

--- list/resize/main.cpp
+++ list/resize/main.cpp
@@ -9,9 +9,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_list()
+void print_list(const auto & str,const auto & lis) {
+    for (cout<<str<<":";const auto & i:lis)
+        {
+            cout<<i<<" ";
+        }
+    cout<<"\n";
+}
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis =list{1,2,3,4};
+    print_list("first",lis);
+    lis.resize(6);
+    print_list("resize 6",lis);
+    lis.resize(2);
+    print_list("resize 2",lis);
+    lis.resize(9,4);
+    print_list("resize 9 4",lis);
     return 0;
 }
