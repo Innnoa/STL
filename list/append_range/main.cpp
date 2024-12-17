7afedb226ca05c159160fe3914b7f4a415348d54
innno <banxiaqaq@gmail.com>
2024-12-17 09:17:55 +0800

    1

--- list/append_range/main.cpp
+++ list/append_range/main.cpp
@@ -11,6 +11,13 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    auto
+    auto lis = list{1,2,3,4};
+    const auto vec = vector{4,5,6,7};
+#ifdef __cpp_lib_containers_ranges
+    lis.append_range(vec);
+#else
+    lis.insert(lis.end(),vec.begin(),vec.end());
+#endif
+    assert((lis==list{1,2,3,4,4,5,6,7}));
     return 0;
 }
