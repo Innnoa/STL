74b1c8dd720806f71d1bea2c9005fe6c0ca0eec7
innno <banxiaqaq@gmail.com>
2024-12-17 09:32:04 +0800

    1

--- list/push_front/main.cpp
+++ list/push_front/main.cpp
@@ -11,5 +11,15 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis = list<string>{};
+    lis.push_front("zjy");
+    string str = "toast.";
+    lis.push_front(move(str));
+    for (cout << "all:\n"; const auto &i: lis)
+        {
+            cout << quoted(i) << " ";
+        }
+    cout <<"str:"<<quoted(str)<< '\n';
+
     return 0;
 }
