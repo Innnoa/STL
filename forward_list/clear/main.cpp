ad4fec0af0e033c7b792bc3f2e1ca128e7eb3de7
innno <banxiaqaq@gmail.com>
2024-12-18 11:03:02 +0800

    1

--- forward_list/clear/main.cpp
+++ forward_list/clear/main.cpp
@@ -9,7 +9,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print_forward_list(const auto & str,const auto & for_lis) {
+    for (cout<<str<<"{";const auto & i:for_lis)
+        {
+            cout<<i<<" ";
+        }
+    cout<<"}\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto for_lis =forward_list {1,2,3,4,5};
+    print_forward_list("before:",for_lis);
+    for_lis.clear();
+    print_forward_list("after",for_lis);
     return 0;
 }
