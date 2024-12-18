7ffb3d93010f03561255e868c4f86bbbb2b68688
innno <banxiaqaq@gmail.com>
2024-12-18 10:57:05 +0800

    1

--- forward_list/max_size/main.cpp
+++ forward_list/max_size/main.cpp
@@ -11,5 +11,12 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    std::forward_list<char> p;
+    std::forward_list<long> q;
+    std::cout << std::uppercase
+              << "p.max_size() = " << std::dec << p.max_size() << " = 0x"
+              << std::hex << p.max_size() << '\n'
+              << "q.max_size() = " << std::dec << q.max_size() << " = 0x"
+              << std::hex << q.max_size() << '\n';
     return 0;
 }
