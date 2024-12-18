dbcee4ae8724cca4b8e39e8507a86f1f8f75048e
innno <banxiaqaq@gmail.com>
2024-12-18 08:51:52 +0800

    1

--- list/sort/main.cpp
+++ list/sort/main.cpp
@@ -1,4 +1,4 @@
-    /**
+/**
  * ClassName: ${NAME}
  * package: ${PACKAGE_NAME}
  * Description:
@@ -9,7 +9,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+ostream &operator<<(ostream &ostr, const list<int> &list) {
+    for (const int i: list)
+        ostr << ' ' << i;
+    return ostr;
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto lis = list{2, 4, 11, 5, 6, 2, 3, 7, 1, 23, 1};
+    cout << "before:" << lis << "\n";
+    lis.sort();
+    cout << "after:" << lis << "\n";
+    lis.sort(greater<>());
+    cout << "after:" << lis << "\n";
     return 0;
 }
