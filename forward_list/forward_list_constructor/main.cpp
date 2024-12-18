3ff92ac8e85c07974dac125f721a113026607706
innno <banxiaqaq@gmail.com>
2024-12-18 09:39:39 +0800

    1

--- forward_list/forward_list_constructor/main.cpp
+++ forward_list/forward_list_constructor/main.cpp
@@ -9,7 +9,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename T>
+ostream &operator<<(ostream &s, const forward_list<T> &v) {
+    s.put('{');
+    for (char comma[]{'\0', ' ', '\0'}; const auto &i: v)
+        {
+            s << comma << i, comma[0] << ',';
+        }
+    return s << "}\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto for_lis = forward_list<string>{"hello", "world"};
+    cout << "1: " << for_lis;
+    auto for_lis_1 = forward_list(for_lis.begin(), for_lis.end());
+    cout << "2: " << for_lis_1;
+    auto for_lis_2 = forward_list(for_lis);
+    cout<<"3: "<<for_lis_2;
+    auto for_lis_3 = forward_list<string> (5,"zjy");
+    cout<<"4: "<<for_lis_3;
+    const auto rg = {"where","you","are"};
+#ifdef __cpp_lib_containers_ranges
+    forward_list<string> for_lis_4(from_range,rg);
+#else
+    forward_list<string> for_lis_4(rg.begin(),rg.end());
+#endif
+    cout<<"5: "<<for_lis_4;
+
     return 0;
 }
