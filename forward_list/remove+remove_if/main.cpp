02ee60b37776780c4a4b67e0f99d701ea0b2b821
innno <banxiaqaq@gmail.com>
2024-12-19 08:41:30 +0800

    1

--- forward_list/remove+remove_if/main.cpp
+++ forward_list/remove+remove_if/main.cpp
@@ -11,5 +11,16 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    forward_list<int> for_lis {1,2,3,4,5,6,7,8,9,10,11};
+    auto count1 =for_lis.remove(1);
+    cout<<"remove element:"<<count1;
+    auto count2 = for_lis.remove_if([](const int n){return n>10;});
+    cout<<"\nremove_if >10:"<<count2;
+    cout<<"\nall:";
+    for (const auto & i:for_lis)
+        {
+            cout<<i<<" ";
+        }
+    cout<<"\n";
     return 0;
 }
