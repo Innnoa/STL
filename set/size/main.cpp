50dbe4dafe5bbc6ee5f1bc3f7089783ec3b8354a
innno <banxiaqaq@gmail.com>
2024-12-19 14:50:42 +0800

    1

---
+++ set/size/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto se =set {1,2,3,4};
+    cout<<"nums:"<<se.size()<<"\n";
+    se.clear();
+    cout<<"nums:"<<se.size();
+    return 0;
+}
