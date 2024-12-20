e1dcf650df25404a3dd1327bd595e2940f3ac5b0
innno <banxiaqaq@gmail.com>
2024-12-20 08:16:00 +0800

    1

--- set/extract/main.cpp
+++ set/extract/main.cpp
@@ -9,7 +9,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print_set(const auto & str,const auto & se) {
+    for (cout<<str<<":";const auto & i:se)
+        {
+            cout<<i<<" ";
+        }
+   cout<<"\n";
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto se =set{1,2,3,4,5};
+    print_set("before: ",se);
+    auto nh = se.extract(1);
+    nh.value()=666;
+    print_set("before and after:",se);
+    se.insert(move(nh));
+    print_set("after",se);
     return 0;
 }
