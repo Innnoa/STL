0e1855826303738993b11d4c94acef1acb536f9e
innno <banxiaqaq@gmail.com>
2024-12-24 14:58:40 +0800

    1

---
+++ queue/back/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto que = queue<int>{};
+    que.push(1);
+    que.push(2);
+    que.push(3);
+    while (!que.empty())
+        {
+            cout << que.back() << " ";
+            que.pop();
+        }
+    return 0;
+}
