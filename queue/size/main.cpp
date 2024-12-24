3100bd1d08158de7f5da4c4bfb9768dc82941c9d
innno <banxiaqaq@gmail.com>
2024-12-24 15:04:26 +0800

    1

---
+++ queue/size/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * ClassName: ${NAME}
+ * package: ${PACKAGE_NAME}
+ * Description:
+ * @Author: innno
+ * @Create: 2024/2/27 - 21:54
+ * @Version: v1.0
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    auto que = queue<int>{};
+    cout << que.size() << " ";
+    que.push(1);
+    que.push(2);
+    cout << que.size();
+    return 0;
+}
